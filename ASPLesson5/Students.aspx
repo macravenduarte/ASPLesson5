7141eb0d8d9d23c48eccb3be60271d510b9df280
macravenduarte <duarte.marco242@gmail.com>
2016-06-12 21:50:54 -0400

    Added Delete functionality to Students

--- ASPLesson5/Students.aspx
+++ ASPLesson5/Students.aspx
@@ -7,14 +7,20 @@
                 <h1>Student List</h1>
                 <a href="StudentDetails.aspx" class="btn btn-success btn-sm"><i class="fa fa-plus"></i> Add Student</a>
                 <asp:GridView runat="server" CssClass="table table-bordered table-striped table-hover" 
-                    ID="StudentsGridView" AutoGenerateColumns="false">
+                    ID="StudentsGridView" AutoGenerateColumns="false" DataKeyNames="StudentID"
+                    OnRowDeleting="StudentsGridView_RowDeleting" AllowPaging="true" PageSize="3"
+                     OnPageIndexChanging="StudentsGridView_PageIndexChanging">
                     <Columns>
                         <asp:BoundField DataField="StudentID" HeaderText="Student ID" Visible="true" />
                         <asp:BoundField DataField="LastName" HeaderText="Last Name" Visible="true" />
                         <asp:BoundField DataField="FirstMidName" HeaderText="First Name" Visible="true" />
                         <asp:BoundField DataField="EnrollmentDate" HeaderText="Enrollment Date" Visible="true"
                             DataFormatString="{0:MMM dd, yyyy}" />
+                        <asp:CommandField HeaderText="Delete" DeleteText="<i class='fa fa-trash-o fa-lg'></i> Delete"  
+                            ShowDeleteButton="true" ButtonType="Link" ControlStyle-CssClass="btn btn-danger btn-sm" />
+                        
                     </Columns>
+           
                 </asp:GridView>
             </div>
         </div>
--- ASPLesson5/Students.aspx.cs
+++ ASPLesson5/Students.aspx.cs
@@ -45,5 +45,59 @@ namespace ASPLesson5
                 StudentsGridView.DataBind();
             }
         }
+
+        /// <summary>
+        /// This Event Handler deletes a Student fro the db usinf EF
+        /// </summary>
+        /// 
+        /// @method StudentsGridView_RowDeleting
+        /// @param (object) sender
+        /// @params(GridViewDeleteEventArgs) e
+        /// @returns void
+        /// 
+        protected void StudentsGridView_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            //Stre whish row was clicked
+            int selectedRow = e.RowIndex;
+
+            //Get the selected StudentID using the Grid's DataKEy collection
+            int StudentID = Convert.ToInt32(StudentsGridView.DataKeys[selectedRow].Values["StudentID"]);
+
+            //Use EF to find the selected student in the DB and remove it
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                //Create object of the student class and store the query string inside of it
+                Student deletedStudent = (from studentRecords in db.Students
+                                          where studentRecords.StudentID == StudentID
+                                          select studentRecords).FirstOrDefault();
+
+                //Remove the selected student from the db
+                db.Students.Remove(deletedStudent);
+
+                //Save changes back to the db
+                db.SaveChanges();
+
+                //Refresh the grid
+                this.GetStudents();
+
+            }
+        }
+
+        /// <summary>
+        /// This Event Handler allows pagination to occur for the Students page        ///
+        /// </summary>
+        /// 
+        /// @method StudentsGridView_PageIndexChanging
+        /// @param (object) sender
+        /// @params(GridViewPageEventArgs) e
+        /// @returns void
+        protected void StudentsGridView_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            //Set the new page number
+            StudentsGridView.PageIndex = e.NewPageIndex;
+
+            //Refresh the grid
+            this.GetStudents();
+        }
     }
 }
